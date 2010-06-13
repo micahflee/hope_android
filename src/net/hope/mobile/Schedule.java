3a27cfcbc0315585dccad303ee7a596b3035b914
Micah Lee <micahflee@gmail.com>
2010-06-12 18:42:20 -0700

    Initial commit

---
+++ src/net/hope/mobile/Schedule.java
@@ -0,0 +1,20 @@
+package net.hope.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Schedule extends Activity {
+	WebView webview;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("file:///android_asset/www/schedule.html");
+    }
+}
